c08d9b0f108a4690774304b9234686c7ca24a7d3
Andrew Vardeman <andrewdev@the-vardemans.net>
2017-10-28 18:28:57 -0500

    added a command line processor

---
+++ src/FileSharper/FileSharperCore/Processors/CommandLineProcessor.cs
@@ -0,0 +1,44 @@
+﻿// Copyright (c) 2017 Andrew Vardeman.  Published under the MIT license.
+// See license.txt in the FileSharper distribution or repository for the
+// full text of the license.
+
+using System;
+using System.IO;
+using System.Threading;
+using FileSharperCore.Util;
+
+namespace FileSharperCore.Processors
+{
+
+    public class CommandLineParameters
+    {
+        public string CommandLine { get; set; } = "echo \"{FullName}\"";
+    }
+
+    public class CommandLineProcessor : SingleFileProcessorBase
+    {
+        private CommandLineParameters m_Parameters = new CommandLineParameters();
+
+        public override string Name => "Execute command line";
+
+        public override string Description => "Executes the specified command line";
+
+        public override object Parameters => m_Parameters;
+
+        public override ProcessingResult Process(FileInfo file, string[] values, CancellationToken token)
+        {
+            ProcessingResultType resultType = ProcessingResultType.Failure;
+            try
+            {
+                string comandLine = ReplaceUtil.Replace(m_Parameters.CommandLine, file);
+                System.Diagnostics.Process.Start("cmd", "/c " + comandLine);
+                resultType = ProcessingResultType.Success;
+            }
+            catch (Exception ex)
+            {
+                    
+            }
+            return new ProcessingResult(resultType, null);
+        }
+    }
+}
