62b2e7a2a9e4cf098f147e41777529ce35b6965e
Andrew Vardeman <andrewdev@the-vardemans.net>
2017-10-28 16:09:34 -0500

    Added a "Move to Recycle Bin" processor.

---
+++ src/FileSharper/FileSharperCore/Processors/RecycleProcessor.cs
@@ -0,0 +1,26 @@
+﻿// Copyright (c) 2017 Andrew Vardeman.  Published under the MIT license.
+// See license.txt in the FileSharper distribution or repository for the
+// full text of the license.
+
+using System.IO;
+using System.Threading;
+using Microsoft.VisualBasic.FileIO;
+
+namespace FileSharperCore.Processors
+{
+    public class RecycleProcessor : SingleFileProcessorBase
+    {
+        public override string Name => "Move to Recycle Bin";
+
+        public override string Description => "Move the file to the Recycle Bin";
+
+        public override object Parameters => null;
+
+        public override ProcessingResult Process(FileInfo file, string[] values, CancellationToken token)
+        {
+            FileSystem.DeleteFile(file.FullName, UIOption.OnlyErrorDialogs, RecycleOption.SendToRecycleBin,
+                UICancelOption.DoNothing);
+            return null;
+        }
+    }
+}
