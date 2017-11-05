55ed9d2807f0e202e1b71f4ebf2b0b0347d4ad13
Andrew Vardeman <andrewdev@the-vardemans.net>
2017-11-04 20:05:00 -0500

    added license and removed unused usings

--- src/FileSharper/FileSharperCore/Util/TextUtil.cs
+++ src/FileSharper/FileSharperCore/Util/TextUtil.cs
@@ -1,8 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿// Copyright (c) 2017 Andrew Vardeman.  Published under the MIT license.
+// See license.txt in the FileSharper distribution or repository for the
+// full text of the license.
+
+using System;
 
 namespace FileSharperCore.Util
 {
