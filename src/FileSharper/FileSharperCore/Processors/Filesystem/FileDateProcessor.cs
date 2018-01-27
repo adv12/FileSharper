809e88587335e4f24d22b98236b05e5996dc887b
Andrew Vardeman <andrewdev@the-vardemans.net>
2018-01-27 16:48:14 -0600

    Added a FileDateProcessor for setting file dates

---
+++ src/FileSharper/FileSharperCore/Processors/Filesystem/FileDateProcessor.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using Xceed.Wpf.Toolkit.PropertyGrid.Attributes;
+
+namespace FileSharperCore.Processors.Filesystem
+{
+    public class FileDateProcessorParameters
+    {
+        [PropertyOrder(1, UsageContextEnum.Both)]
+        public FileDateType FileDateType { get; set; } = FileDateType.Modified;
+        [PropertyOrder(2, UsageContextEnum.Both)]
+        public DateTime Date { get; set; } = DateTime.Now;
+    }
+
+    public class FileDateProcessor : SingleFileProcessorBase
+    {
+        private FileDateProcessorParameters m_Parameters = new FileDateProcessorParameters();
+
+        public override string Name => "Set file date";
+
+        public override string Category => "Filesystem";
+
+        public override string Description => "Sets the specified file date property to the specified date";
+
+        public override object Parameters => m_Parameters;
+
+        public override ProcessingResult Process(FileInfo file, string[] values,
+            IProgress<ExceptionInfo> exceptionProgress, CancellationToken token)
+        {
+            switch (m_Parameters.FileDateType)
+            {
+                case FileDateType.Accessed:
+                    File.SetLastAccessTime(file.FullName, m_Parameters.Date);
+                    break;
+                case FileDateType.Created:
+                    File.SetCreationTime(file.FullName, m_Parameters.Date);
+                    break;
+                case FileDateType.Modified:
+                    File.SetLastWriteTime(file.FullName, m_Parameters.Date);
+                    break;
+            }
+            return new ProcessingResult(ProcessingResultType.Success, "Success", new FileInfo[] { file });
+        }
+    }
+}
