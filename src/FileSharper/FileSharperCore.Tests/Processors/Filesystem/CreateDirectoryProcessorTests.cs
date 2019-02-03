dcd57d31b97476a77bb50a57d607dc8598508cf0
Andrew Vardeman <andrewdev@the-vardemans.net>
2019-02-02 21:56:39 -0600

    Added CreateDirectoryProcessor and associated test class

---
+++ src/FileSharper/FileSharperCore.Tests/Processors/Filesystem/CreateDirectoryProcessorTests.cs
@@ -0,0 +1,35 @@
+﻿// Copyright (c) 2019 Andrew Vardeman.  Published under the MIT license.
+// See license.txt in the FileSharper distribution or repository for the
+// full text of the license.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using FileSharperCore.Processors.Filesystem;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FileSharperCore.Tests.Processors.Filesystem
+{
+    [TestClass]
+    public class CreateDirectoryProcessorTests : TestBase
+    {
+        [TestMethod]
+        public void Success()
+        {
+            CreateDirectoryProcessor processor = new CreateDirectoryProcessor();
+            processor.SetParameter("DirectoryPath", GetCurrentTestResultsFilePath("Subdir"));
+            FileInfo file = GetTestFile("BasicTextFile.txt");
+            processor.Init(RunInfo);
+            ProcessingResult result = processor.Process(file, MatchResultType.Yes,
+                new string[0], new FileInfo[0], ProcessInput.OriginalFile,
+                CancellationToken.None);
+            processor.Cleanup();
+            Assert.AreEqual(ProcessingResultType.Success, result.Type);
+            Assert.AreEqual(0, result.OutputFiles.Length);
+        }
+    }
+}
---
+++ src/FileSharper/FileSharperCore/Processors/Filesystem/CreateDirectoryProcessor.cs
@@ -0,0 +1,56 @@
+﻿// Copyright (c) 2019 Andrew Vardeman.  Published under the MIT license.
+// See license.txt in the FileSharper distribution or repository for the
+// full text of the license.
+
+using System;
+using System.IO;
+using System.Threading;
+using FileSharperCore.Util;
+
+namespace FileSharperCore.Processors.Filesystem
+{
+    public class CreateDirectoryParameters
+    {
+        public string DirectoryPath { get; set; } = @"{DirectoryName}\NewSubdirectory";
+    }
+
+    public class CreateDirectoryProcessor : SingleFileProcessorBase
+    {
+        private CreateDirectoryParameters m_Parameters = new CreateDirectoryParameters();
+
+        public override string Category => "Filesystem";
+
+        public override string Name => "Create directory";
+
+        public override string Description => "Create a directory if it does not exist";
+
+        public override object Parameters => m_Parameters;
+
+        public override ProcessingResult Process(FileInfo file, string[] values, CancellationToken token)
+        {
+            string directoryPath = ReplaceUtil.Replace(m_Parameters.DirectoryPath, file);
+            ProcessingResultType resultType = ProcessingResultType.Failure;
+            string message = "Failed to create directory";
+            try
+            {
+                if (Directory.Exists(directoryPath))
+                {
+                    resultType = ProcessingResultType.Success;
+                    message = "Directory already exists";
+                }
+                else if (!File.Exists(directoryPath))
+                {
+                    Directory.CreateDirectory(directoryPath);
+                    resultType = ProcessingResultType.Success;
+                    message = "Directory created";
+                }
+            }
+            catch (Exception ex)
+            {
+                RunInfo.ExceptionInfos.Enqueue(new ExceptionInfo(ex, file));
+                message = ex.Message;
+            }
+            return new ProcessingResult(resultType, message, new FileInfo[0]);
+        }
+    }
+}
